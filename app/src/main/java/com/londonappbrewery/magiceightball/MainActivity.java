7be7dca75b628c354e7a8c99f67f96fae74ab547
wiz <wiz_kidz@outlook.com>
2019-03-12 22:43:10 -0500

    Complete 8ball challenge

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button askBtn = findViewById(R.id.btn_ask);
+        final ImageView ballImage = findViewById(R.id.image_ball);
+
+        final int[] balls = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        askBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random picNum = new Random();
+                int val = picNum.nextInt(5);
+                 Log.v("8ball"," Random number - "+val);
+                 ballImage.setImageResource(balls[val]);
+            }
+        });
+
+
     }
 }
